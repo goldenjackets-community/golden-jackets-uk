6977fea4b191b2c0ebe681fb6a0397e898bb2bab
Ricardo Gulias <gulias@DAREDE-001430.darede.corp>
2026-05-17 10:40:48 -0300

    Add UK chapter site (adapted from Poland template)

---
+++ config.js
@@ -0,0 +1,5 @@
+var GJ_CONFIG = {
+  ADMIN_API: 'https://kqiq2bltjd.execute-api.us-east-1.amazonaws.com/admin',
+  ADMIN_EMAILS: ['tz0py@yahoo.com', 'ricardo.gulias@darede.com.br'],
+  CHAPTER: 'uk'
+};
